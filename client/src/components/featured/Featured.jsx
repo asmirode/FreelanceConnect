f1c20e8ae8546509e3a56e2a0d428536d2c8af60
Asmi Rode <asmirode17847@gmail.com>
2025-09-28 20:07:15 +0530

    search bar fix

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -6,9 +6,11 @@ function Featured() {
   const [input, setInput] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     navigate(`/gigs?search=${input}`);
   };
+
   return (
     <div className="featured">
       <div className="container">
@@ -16,27 +18,21 @@ function Featured() {
           <h1>
             Find the perfect <span>freelance</span> services for your business
           </h1>
-          <div className="search">
+          <form className="search" onSubmit={handleSubmit}>
             <div className="searchInput">
-              <img src="./img/search.png" alt="" />
+              <img src="/img/search.png" alt="" />
               <input
                 type="text"
-                placeholder='Try "building mobil app"'
+                placeholder='Try "building mobile app"'
+                value={input}
                 onChange={(e) => setInput(e.target.value)}
               />
             </div>
-            <button onClick={handleSubmit}>Search</button>
-          </div>
-          <div className="popular">
-            <span>Popular:</span>
-            <button>Web Design</button>
-            <button>WordPress</button>
-            <button>Logo Design</button>
-            <button>AI Services</button>
-          </div>
+            <button type="submit">Search</button>
+          </form>
         </div>
         <div className="right">
-          <img src="./img/man.png" alt="" />
+          <img src="/img/mman.png" alt="" />
         </div>
       </div>
     </div>
