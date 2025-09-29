052c4e544f7e74064c0a0d664c6893044bc81155
Asmi Rode <asmirode17847@gmail.com>
2025-09-29 09:13:33 +0530

    removed cards from home

--- client/src/components/projectCard/ProjectCard.jsx
+++ client/src/components/projectCard/ProjectCard.jsx
@@ -3,16 +3,17 @@ import "./ProjectCard.scss";
 
 function ProjectCard({ card }) {
   return (
-    <div className="projectCard">
-      <img src={card.img} alt="" />
-      <div className="info">
-        <img src={card.pp} alt="" />
-        <div className="texts">
-          <h2>{card.cat}</h2>
-          <span>{card.username}</span>
-        </div>
-      </div>
-    </div>
+    // <div className="projectCard">
+    //   <img src={card.img} alt="" />
+    //   <div className="info">
+    //     <img src={card.pp} alt="" />
+    //     <div className="texts">
+    //       <h2>{card.cat}</h2>
+    //       <span>{card.username}</span>
+    //     </div>
+    //   </div>
+    // </div>
+    <></>
   );
 }
 
